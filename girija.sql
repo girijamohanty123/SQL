76f1fc18fc7bca18df071a08bf62c5123264971f
girija Mohanty <115263551+girijamohanty123@users.noreply.github.com>
2024-09-03 10:06:33 +0000

    myTestCodes

--- girija.sql
+++ girija.sql
@@ -1,1 +1,1 @@
-
+create Table mytable;
---
+++ testCodes/python.py
@@ -0,0 +1,8 @@
+def india(*a):
+     for i in a:
+         if i== "India":
+             return "you are from India"
+         else:
+             return "you are not indian "
+c=india({"India"})
+print(c)
